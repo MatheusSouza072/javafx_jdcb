7bc85e77f5e32fb717b0f1c6cd69b71514e2ab3a
Matheuskbrasil <matheus.070202@gmail.com>
2019-07-15 13:52:30 -0300

    first commit

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -2,14 +2,9 @@ package model.services;
 
 import java.util.List;
 
-import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 
-
-
-	
 }
- 
