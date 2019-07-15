e0cac71a777a7237fd65a8497d8ca081ac5d25c3
Matheuskbrasil <matheus.070202@gmail.com>
2019-07-15 14:10:10 -0300

    Department Controller

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,30 +1,8 @@
 package gui;
 
+import javafx.collections.ObservableList;
+import model.entities.Department;
+
 public class DepartmentListController {
-	/*
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 */
+	private ObservableList<Department> obsList;
 } 
