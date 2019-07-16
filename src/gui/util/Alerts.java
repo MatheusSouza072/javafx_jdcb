606bfebd687b21ad02328f80bb572609a5a98825
Matheuskbrasil <matheus.070202@gmail.com>
2019-07-16 14:55:37 -0300

    Seller Controller

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,3 +1,4 @@
+
 package gui.util;
 
 import java.util.Optional;
@@ -7,6 +8,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 
 public class Alerts {
+
 	public static void showAlert(String title, String header, String content, AlertType type) {
 		Alert alert = new Alert(type);
 		alert.setTitle(title);
--- src/model/exceptions/ValidationException.java
+++ src/model/exceptions/ValidationException.java
@@ -20,4 +20,4 @@ public class ValidationException extends RuntimeException {
 	public void addError(String fieldName, String errorMessage) {
 		errors.put(fieldName, errorMessage);
 	}
-} 
+}
